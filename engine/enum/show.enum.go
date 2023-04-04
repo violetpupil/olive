e2cde613ee981573bf2f027f609569003a5dbe9d
luxcgo <luxcgo@gmail.com>
2023-04-05 01:04:54 +0800

    go.mod: fix #134

--- engine/enum/show.enum.go
+++ engine/enum/show.enum.go
@@ -3,11 +3,11 @@ package enum
 type ShowTaskStatusID = uint32
 
 var ShowTaskStatus = struct {
-	Absent      ShowTaskStatusID
-	Monitoring  ShowTaskStatusID
-	Downloading ShowTaskStatusID
+	Absent     ShowTaskStatusID
+	Monitoring ShowTaskStatusID
+	Recording  ShowTaskStatusID
 }{
-	Absent:      0,
-	Monitoring:  1,
-	Downloading: 2,
+	Absent:     0,
+	Monitoring: 1,
+	Recording:  2,
 }
