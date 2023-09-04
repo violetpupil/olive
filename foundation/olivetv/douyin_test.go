ffc59c7d4a9273c6170e2187471d6c399cc683d1
luxcgo <luxcgo@gmail.com>
2023-09-04 22:20:19 +0800

    olivetv: fix douyin

--- foundation/olivetv/douyin_test.go
+++ foundation/olivetv/douyin_test.go
@@ -3,13 +3,13 @@ package olivetv_test
 import (
 	"testing"
 
-	tv "github.com/go-olive/olive/foundation/olivetv"
+	"github.com/go-olive/olive/foundation/olivetv"
 )
 
 func TestDouyin_Snap(t *testing.T) {
-	u := "https://live.douyin.com/278246244716"
-	cookie := "__ac_nonce=062f60f640020c76e5b7c; __ac_signature=_02B4Z6wo00f011BxI-gAAIDAxKd45w5q9dNQUSdAALb1WEJMf.7Ma1NuqG0oiO7cYko5mx60CrOZ7DEMeiImZWCkLuxGYUV7nL8NBVxgyWeCnQdWfhttJNV21omp7bThIi8SVu-58ihu3EiU32;"
-	dy, err := tv.NewWithURL(u, tv.SetCookie(cookie))
+	u := "https://live.douyin.com/80017709309"
+	// cookie := `__ac_nonce=06487158a00d5c2d4634c; __ac_signature=_02B4Z6wo00f01CjqXDgAAIDDeJ0YwjlhZKAoyliAAG7EcZEUW2MdeGRISr2fobBUEzpAtX24xyQL5JQQzCHcosKQXhCMWI0W4MacAadtfeEO0QnuggwfMl6vwiakM6a3ROFyKBXeWrGE1FRZ87;`
+	dy, err := olivetv.NewWithURL(u)
 	if err != nil {
 		println(err.Error())
 		return
